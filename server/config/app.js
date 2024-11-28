71d54344cd240b35cb2f76792538a8cfe9822e9f
Fatima-sohail123 <fatima.sohail@ontariotechu.net>
2024-11-28 00:02:40 -0500

    created  login, register and edited index.js

--- server/config/app.js
+++ server/config/app.js
@@ -3,25 +3,52 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let session = require('express-session');
+let passport = require('passport');
+let passportLocal = require('passport-local');
+let flash = require('connect-flash');
+let cors = require('cors');
+let mongoose = require('mongoose');
 
+// Define the User model
+let userModel = require('../model/User');
+let User = userModel.User;
+
+// Initialize Passport strategy AFTER defining User
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Define routes
 let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users');
-let bookRouter = require('../routes/pet')
+let petRouter = require('../routes/pet');
 
-let app = express();
-let mongoose = require('mongoose');
+// MongoDB connection
 let DB = require('./db');
-// point my mongoose to the URI
 mongoose.connect(DB.URI);
 let mongoDB = mongoose.connection;
-mongoDB.on('error',console.error.bind(console,'Connection Error'))
-mongoDB.once('open',()=>{
-  console.log('MongoDB Connected')
-})
-mongoose.connect(DB.URI,{useNewURIParser:true,
-  useUnifiedTopology:true
-})
-// view engine setup
+mongoDB.on('error', console.error.bind(console, 'Connection Error'));
+mongoDB.once('open', () => {
+  console.log('MongoDB Connected');
+});
+
+let app = express();
+
+// Set up express-session
+app.use(
+  session({
+    secret: 'SomeSecret',
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+// Initialize flash and passport
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
@@ -31,23 +58,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
+
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/petslist',bookRouter);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use('/petslist', petRouter);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// Error handler
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error',{title:'Error'});
+  res.render('error', { title: 'Error' });
 });
 
 module.exports = app;
