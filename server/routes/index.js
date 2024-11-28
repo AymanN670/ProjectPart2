3452baf932725247b4e353a77d83ecb3acc3e689
AymanN670 <ayman.najmuddin@ontariotechu.net>
2024-11-28 00:06:43 -0500

    made fatima's changes + edited login.ejs file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const passport=require('passport');
+const DB = require('../config/db');
+let userModel = require('../model/User');
+let User = userModel.User;
 
 /* GET index page. */
 router.get('/', function(req, res, next) {
@@ -37,5 +41,100 @@ router.get('/contactus', function(req, res, next) {
     displayName: req.user ? req.user.displayName: ''
    });
 });
+
+router.get('/login', function(req, res, next){
+  // built in in passport so it uses small u
+  if(!req.user)
+  {
+    res.render('Auth/login',
+      {
+        title: 'Login',
+        displayName: req.user ? req.user.displayName:'',
+        message:req.flash('loginMessage')
+
+      }
+    )
+  }
+  else
+  {
+    return res.redirect('/')
+  }
+})
+router.post('/login', function(req, res, next){
+  passport.authenticate('local', (err, user, info)=>{
+    if(err)
+    {
+      return next(err)
+    }
+    if(!user)
+    {
+      req.flash('loginMessage','Authentication Error')
+      return res.redirect('/login');
+    }
+
+    req.login(user, (err)=>{
+      if (err)
+      {
+        return next(err)
+      }
+      return res.redirect('/petslist')
+    })
+  }) (req, res, next)
+})
+router.get('/register', function(req,res,next){
+  if (!req.user)
+  {
+    res.render('Auth/register',{
+      title: 'Register',
+      displayName: req.user? req.user.displayName:'',
+      message:req.flash('registerMessage'),
+    })
+  }
+  else{
+    return res.redirect('/')
+  }
+})
+router.post('/register', function(req, res, next){
+  let newUser = new User({
+    username: req.body.username,
+    //password: req.body.password,
+    email: req.body.email,
+    displayName: req.body.displayName
+
+  })
+  User.register(newUser, req.body.password, (err) =>{
+    if(err)
+    {
+      console.log("Error: Inserting the new User");
+      if(err.name=="UserExistError")
+      {
+        req.flash('registerMessage',
+          'Registration Error: User already exists')
+      
+      }
+      return res.render('Auth/register',{
+        title:'Register',
+        displayName:req.user? req.user.displayName:'',
+        message: req.flash('registerMessage')
+      })
+    }
+    else
+    {
+      return passport.authenticate('local')(req,  res, ()=>{
+        res.redirect('/petslist')
+      })
+    }
+
+  })
+})
+router.get('/logout', function(req, res, next){
+  req.logOut(function(err){
+    if(err)
+    {
+      return next(err);
+    }
+  })
+  res.redirect('/')
+})
 
 module.exports = router;
